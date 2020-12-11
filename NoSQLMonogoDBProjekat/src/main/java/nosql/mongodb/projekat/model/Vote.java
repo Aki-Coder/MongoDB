da9e03a9c15714ac4e0421ac247ccab4d38abcc6
Aki-Coder <anchicabeybe@gmail.com>
2020-12-11 20:43:06 +0100

    maven project,classes,parsing

---
+++ NoSQLMonogoDBProjekat/src/main/java/nosql/mongodb/projekat/model/Vote.java
@@ -0,0 +1,24 @@
+package nosql.mongodb.projekat.model;
+
+public class Vote {
+	
+	private double average;
+	private int count;
+	
+	
+	public double getAverage() {
+		return average;
+	}
+	public void setAverage(double average) {
+		this.average = average;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+
+}
